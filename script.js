10924f7bd561bc8392136854038d9500c1cf216b
Abhishek-Bohora <abhishek.bohora16@gmail.com>
2022-01-26 18:41:14 +0545

    completed

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 const starsEl = document.querySelectorAll(".fa-star");
-
-// console.log(starsEl);
+const emojisEl = document.querySelectorAll(".far");
 
 starsEl.forEach((starsEl, index) => {
   starsEl.addEventListener("click", () => {
-    console.log("clicked", index);
+    // console.log("clicked", index);
+    updateRating(index);
   });
 });
+
+function updateRating(index) {
+  starsEl.forEach((starsEl, indx) => {
+    if (indx < index + 1) {
+      starsEl.classList.add("active");
+    } else {
+      starsEl.classList.remove("active");
+    }
+  });
+
+  emojisEl.forEach((emojiEl) => {
+    emojiEl.style.transform = `translateX(-${index * 50}px)`;
+  });
+}
